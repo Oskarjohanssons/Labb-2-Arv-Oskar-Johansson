6841d70765be39e9f4d17373c13a256e1503495b
Oskar Johansson <oskar.tzar@hotmail.com>
2023-10-27 07:35:33 +0200

    Add project files.

---
+++ Labb 2-Arv-Oskar Johansson/Animals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_2_Arv_Oskar_Johansson
+{
+    public class Animals
+    {
+        public int Age;
+        public string Name;
+        public string Species;
+        public string FavoriteFood;
+        public string Roar;
+
+        public void Makesound()
+        {
+            Console.WriteLine($"The {Species} is saying {Roar}");
+        }
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} is eating {FavoriteFood} yummy!!");
+        }
+        public void Sleep() 
+        {         
+            Console.WriteLine($"{Name} is sleeping.");
+        }
+
+
+    }
+    public class Dog : Animals
+    {
+
+    }
+    public class Frog : Animals
+    {
+
+    }
+    public class Ape : Animals
+    {
+
+    }
+
+}
---
+++ Labb 2-Arv-Oskar Johansson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb_2_Arv_Oskar_Johansson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
